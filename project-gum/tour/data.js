3dd44ba23c86315b15ffce996f319bb0dd1c2ddd
chabanydesign-netizen <chabanydesign@gmail.com>
2025-12-30 18:06:04 +0200

    test1

---
+++ project-gum/tour/data.js
@@ -0,0 +1,90 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-scene-5",
+      "name": "Scene 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7702386957666807,
+          "pitch": -0.013433368763998033,
+          "rotation": 0,
+          "target": "1---"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1359104800998097,
+          "pitch": 0,
+          "title": "Title",
+          "text": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">https://app.klingai.com/global/image-to-video/multi-id/new</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "1---",
+      "name": "Дитяча розетки копія",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.15757558387119985,
+          "pitch": 0.14421278693021122,
+          "rotation": 0,
+          "target": "0-scene-5"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
