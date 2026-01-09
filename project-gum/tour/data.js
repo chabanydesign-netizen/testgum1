907b834c28c0ff38fc29e80e17afd803a2d2f79a
chabanydesign-netizen <chabanydesign@gmail.com>
2026-01-09 15:40:55 +0200

    зміна туру

--- project-gum/tour/data.js
+++ project-gum/tour/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-08-1",
-      "name": "08 копія1",
+      "id": "0-",
+      "name": "Спальня",
       "levels": [
         {
           "tileSize": 256,
@@ -28,7 +28,52 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -1.8627094676929303,
+          "pitch": 0.12152139262775563,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Вітальня",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2973037205720246,
+          "pitch": 0.10701202973448787,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
       "infoHotspots": []
     }
   ],
