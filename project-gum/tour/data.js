45398b42ba27a20646e3ec1f54fe776e6a85618a
chabanydesign-netizen <chabanydesign@gmail.com>
2026-01-12 23:12:50 +0200

    juj

--- project-gum/tour/data.js
+++ project-gum/tour/data.js
@@ -2,44 +2,6 @@ var APP_DATA = {
   "scenes": [
     {
       "id": "0-",
-      "name": "Спальня",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.8627094676929303,
-          "pitch": 0.12152139262775563,
-          "rotation": 0,
-          "target": "1-"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-",
       "name": "Вітальня",
       "levels": [
         {
@@ -68,8 +30,53 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.2973037205720246,
-          "pitch": 0.10701202973448787,
+          "yaw": -0.2918682916976909,
+          "pitch": 0.0943280359396308,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5474305738498337,
+          "pitch": 0.07035835752329511,
+          "title": "Відео презентація",
+          "text": "<br><a href='https://https://youtu.be/vPVFAOmrO4Q' target='_blank' style='display: inline-block; background-color: #FF0000; color: white; padding: 10px 20px; border-radius: 20px; text-decoration: none; font-weight: bold; font-family: sans-serif;'>▶ Дивитися відео</a>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "Спальня",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8425277685827837,
+          "pitch": 0.10783873869456961,
           "rotation": 0,
           "target": "0-"
         }
