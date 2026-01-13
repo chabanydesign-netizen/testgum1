c4ecb5f19902108376afa677526f748810a4f3a1
chabanydesign-netizen <chabanydesign@gmail.com>
2026-01-13 17:45:23 +0200

    + відео

--- restpark/tour/data.js
+++ restpark/tour/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0--1",
-      "name": "Дитяча кімната1",
+      "id": "0-01",
+      "name": "01",
       "levels": [
         {
           "tileSize": 256,
@@ -24,23 +24,30 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.05160208628135621,
+        "pitch": -0.026037550538674736,
+        "fov": 1.3474042771833745
       },
       "linkHotspots": [
         {
-          "yaw": -0.26208164057838523,
-          "pitch": 0.5422141956636608,
+          "yaw": -0.2896946434348475,
+          "pitch": 0.4817059654690077,
           "rotation": 0,
-          "target": "1--2"
+          "target": "1-02"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 0.31678031133905904,
+          "pitch": 0.08676900184761394,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Відеопрезентаціія</font></font>",
+          "text": "<br><a href='https://www.youtube.com/watch?v=81ADaqaljBU' target='_blank' style='display: inline-block; background-color: #FF0000; color: white; padding: 10px 20px; border-radius: 20px; text-decoration: none; font-weight: bold; font-family: sans-serif;'>▶ Дивитися відео</a>"
+        }
+      ]
     },
     {
-      "id": "1--2",
-      "name": "Дитяча кімната2",
+      "id": "1-02",
+      "name": "02",
       "levels": [
         {
           "tileSize": 256,
@@ -68,10 +75,10 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.31679814121285155,
-          "pitch": 0.5848814312210084,
+          "yaw": -0.3181368612310518,
+          "pitch": 0.4705150042893713,
           "rotation": 0,
-          "target": "0--1"
+          "target": "0-01"
         }
       ],
       "infoHotspots": []
