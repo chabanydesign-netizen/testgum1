80c3428c785a3d6f0a895741148c6a3e1c201d90
chabanydesign-netizen <chabanydesign@gmail.com>
2026-02-15 15:24:47 +0200

    xbccxn

--- restpark_event_hall/tour/data.js
+++ restpark_event_hall/tour/data.js
@@ -20,60 +20,19 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
         }
       ],
-      "faceSize": 2048,
+      "faceSize": 4096,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": 0.044382420269702294,
-          "pitch": 0.14320837334960856,
-          "rotation": 0,
-          "target": "1-02"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-02",
-      "name": "02",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 2.82546130591909,
-          "pitch": 0.07181568663786564,
-          "rotation": 0,
-          "target": "0-01"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     }
   ],
