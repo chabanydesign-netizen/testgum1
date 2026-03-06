1d0401c0aa90d2e7a1478f2059704f8023739ab6
chabanydesign-netizen <chabanydesign@gmail.com>
2026-03-06 12:42:43 +0200

    счи

--- Efekt/tour/data.js
+++ Efekt/tour/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-03",
-      "name": "03",
+      "id": "0-06",
+      "name": "06",
       "levels": [
         {
           "tileSize": 256,
@@ -28,7 +28,52 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 2.9544755613064595,
+          "pitch": 0.06006742004213095,
+          "rotation": 0,
+          "target": "1-05"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-05",
+      "name": "05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6722966172276728,
+          "pitch": 0.035473155092889286,
+          "rotation": 0,
+          "target": "0-06"
+        }
+      ],
       "infoHotspots": []
     }
   ],
