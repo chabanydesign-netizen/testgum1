e754cb6e718ddb6843d2bc37240069e4b7655cf1
chabanydesign-netizen <chabanydesign@gmail.com>
2026-03-18 22:36:24 +0200

    ZCB

--- Efekt/tour/data.js
+++ Efekt/tour/data.js
@@ -30,8 +30,14 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.9544755613064595,
-          "pitch": 0.06006742004213095,
+          "yaw": -1.3870979433296569,
+          "pitch": 0.026192565604009843,
+          "rotation": 0,
+          "target": "2-07"
+        },
+        {
+          "yaw": 2.953874141853266,
+          "pitch": -0.012440969841271965,
           "rotation": 0,
           "target": "1-05"
         }
@@ -68,8 +74,46 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.6722966172276728,
-          "pitch": 0.035473155092889286,
+          "yaw": 0.6668883563467141,
+          "pitch": 0.01634217515724501,
+          "rotation": 0,
+          "target": "0-06"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-07",
+      "name": "07",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0646718160797937,
+          "pitch": 0.22959856619679897,
           "rotation": 0,
           "target": "0-06"
         }
