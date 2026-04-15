1eb304f9c44c931f354533fe851635778a314794
chabanydesign-netizen <chabanydesign@gmail.com>
2026-04-15 15:59:32 +0300

    рр

--- restpark_event_hall/tour/data.js
+++ restpark_event_hall/tour/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-05",
-      "name": "05",
+      "id": "0-02",
+      "name": "02",
       "levels": [
         {
           "tileSize": 256,
@@ -28,108 +28,7 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": -0.10680585662805697,
-          "pitch": 0.14513668967570936,
-          "rotation": 0,
-          "target": "1-06"
-        },
-        {
-          "yaw": 0.04512415043672924,
-          "pitch": 0.1441886822324001,
-          "rotation": 0,
-          "target": "2-07"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-06",
-      "name": "06",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.09630953980206058,
-          "pitch": 0.16870501657460224,
-          "rotation": 0,
-          "target": "0-05"
-        },
-        {
-          "yaw": 0.08356855974990829,
-          "pitch": 0.16890706922712617,
-          "rotation": 0,
-          "target": "2-07"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-07",
-      "name": "07",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.06845887824412777,
-          "pitch": 0.21489201905894006,
-          "rotation": 0,
-          "target": "0-05"
-        },
-        {
-          "yaw": 0.1131363619845196,
-          "pitch": 0.2090297974119757,
-          "rotation": 0,
-          "target": "1-06"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     }
   ],
