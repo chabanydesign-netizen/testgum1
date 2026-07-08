64bbf5f478c7ba62d15af760d5f13714dc98542a
chabanydesign-netizen <chabanydesign@gmail.com>
2026-07-08 16:59:44 +0300

    wrt

--- amsterdam100/tour/data.js
+++ amsterdam100/tour/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-panorama-coridor",
-      "name": "Panorama coridor",
+      "id": "0-koridor-",
+      "name": "Koridor ",
       "levels": [
         {
           "tileSize": 256,
@@ -30,29 +30,35 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.478161606639956,
-          "pitch": 0.04967575590808693,
+          "yaw": -1.4911093391078172,
+          "pitch": 0.020332091339776426,
           "rotation": 0,
-          "target": "7-panorama-bedroom-day"
+          "target": "5-bedroom-day-"
         },
         {
-          "yaw": -1.8084415376608192,
-          "pitch": 0.04785414799797394,
+          "yaw": -1.806410045912152,
+          "pitch": 0.027565236372312896,
           "rotation": 0,
-          "target": "1-kid-bedroom-day2"
+          "target": "3-kid-bedroom-day"
         },
         {
-          "yaw": 1.589634591241647,
-          "pitch": 0.10202651320617839,
+          "yaw": 3.0077862306246423,
+          "pitch": 0.021436978177685972,
           "rotation": 0,
-          "target": "3-panorama-living-room-day1"
+          "target": "1-sv1"
+        },
+        {
+          "yaw": 1.5579619857719136,
+          "pitch": -0.006750859637477902,
+          "rotation": 0,
+          "target": "7-living-room-day-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-kid-bedroom-day2",
-      "name": "Kid bedroom day2",
+      "id": "1-sv1",
+      "name": "sv1",
       "levels": [
         {
           "tileSize": 256,
@@ -80,23 +86,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.000236432136262,
-          "pitch": 0.05855986919522849,
+          "yaw": -1.5960950180935978,
+          "pitch": -0.011623932314710927,
           "rotation": 0,
-          "target": "0-panorama-coridor"
+          "target": "2-sv2"
         },
         {
-          "yaw": -1.4931282367826721,
-          "pitch": -0.015210290871765508,
+          "yaw": -2.8484313100815584,
+          "pitch": 0.08205213137930478,
           "rotation": 0,
-          "target": "6-kid-bedroom-night"
+          "target": "0-koridor-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-panorama-living-room-day-2",
-      "name": "Panorama Living Room day 2",
+      "id": "2-sv2",
+      "name": "sv2",
       "levels": [
         {
           "tileSize": 256,
@@ -124,29 +130,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.1574400642030946,
-          "pitch": 0.07532696249110238,
+          "yaw": 1.5528468806185405,
+          "pitch": 0.019035431955853,
           "rotation": 0,
-          "target": "0-panorama-coridor"
+          "target": "1-sv1"
         },
         {
-          "yaw": -1.520075091180992,
-          "pitch": 0.04596510084555305,
+          "yaw": 1.9623094467524735,
+          "pitch": 0.01463927973534851,
           "rotation": 0,
-          "target": "5-panorama-living-room-night-2"
-        },
-        {
-          "yaw": 3.135038324026251,
-          "pitch": 0.10037884021165588,
-          "rotation": 0,
-          "target": "3-panorama-living-room-day1"
+          "target": "0-koridor-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-panorama-living-room-day1",
-      "name": "Panorama Living Room day1",
+      "id": "3-kid-bedroom-day",
+      "name": "Kid bedroom day",
       "levels": [
         {
           "tileSize": 256,
@@ -174,29 +174,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.0358845545320037,
-          "pitch": 0.05372770536325788,
+          "yaw": 1.0203880692408376,
+          "pitch": 0.0785417878236494,
           "rotation": 0,
-          "target": "0-panorama-coridor"
+          "target": "0-koridor-"
         },
         {
-          "yaw": -1.2806451461214596,
-          "pitch": -0.01654807767463673,
+          "yaw": -1.1340150662315498,
+          "pitch": -0.08269783573657641,
           "rotation": 0,
-          "target": "4-panorama-living-room-night-1"
-        },
-        {
-          "yaw": 0.011400975167040528,
-          "pitch": 0.0392764600568416,
-          "rotation": 0,
-          "target": "2-panorama-living-room-day-2"
+          "target": "4-kid-bedroom-night-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-panorama-living-room-night-1",
-      "name": "Panorama Living Room night 1",
+      "id": "4-kid-bedroom-night-",
+      "name": "Kid bedroom night ",
       "levels": [
         {
           "tileSize": 256,
@@ -224,29 +218,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.0228876954959567,
-          "pitch": 0.04516153195874395,
+          "yaw": -1.2372141990658196,
+          "pitch": -0.05320908482075559,
           "rotation": 0,
-          "target": "0-panorama-coridor"
+          "target": "3-kid-bedroom-day"
         },
         {
-          "yaw": -1.3245038164084342,
-          "pitch": -0.0015797305027085429,
+          "yaw": 1.060879141012915,
+          "pitch": -0.02059470428893917,
           "rotation": 0,
-          "target": "3-panorama-living-room-day1"
-        },
-        {
-          "yaw": 0.050124802372257804,
-          "pitch": 0.044395740080602764,
-          "rotation": 0,
-          "target": "5-panorama-living-room-night-2"
+          "target": "0-koridor-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-panorama-living-room-night-2",
-      "name": "Panorama Living Room night 2",
+      "id": "5-bedroom-day-",
+      "name": "Bedroom day ",
       "levels": [
         {
           "tileSize": 256,
@@ -274,29 +262,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.103051253177008,
-          "pitch": 0.0631813228058018,
+          "yaw": 1.558381771345486,
+          "pitch": 0.004003091153194305,
           "rotation": 0,
-          "target": "0-panorama-coridor"
+          "target": "0-koridor-"
         },
         {
-          "yaw": -1.3745519748289485,
-          "pitch": 0.019353108813213282,
+          "yaw": -0.8305437709071732,
+          "pitch": -0.11685970461372897,
           "rotation": 0,
-          "target": "2-panorama-living-room-day-2"
-        },
-        {
-          "yaw": -3.099784280722165,
-          "pitch": 0.09373526648629849,
-          "rotation": 0,
-          "target": "4-panorama-living-room-night-1"
+          "target": "6-bedroom-night-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "6-kid-bedroom-night",
-      "name": "Kid bedroom night",
+      "id": "6-bedroom-night-",
+      "name": "Bedroom night ",
       "levels": [
         {
           "tileSize": 256,
@@ -324,23 +306,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.0827576689068135,
-          "pitch": 0.07502551444564176,
+          "yaw": -0.7742029436279267,
+          "pitch": -0.07541865560188299,
           "rotation": 0,
-          "target": "0-panorama-coridor"
+          "target": "5-bedroom-day-"
         },
         {
-          "yaw": -1.5529238479035286,
-          "pitch": -0.011506806733876829,
+          "yaw": 1.5361883411573212,
+          "pitch": 0.01700348390972728,
           "rotation": 0,
-          "target": "1-kid-bedroom-day2"
+          "target": "0-koridor-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "7-panorama-bedroom-day",
-      "name": "Panorama Bedroom day",
+      "id": "7-living-room-day-",
+      "name": "Living Room day ",
       "levels": [
         {
           "tileSize": 256,
@@ -368,23 +350,29 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.5866330271250249,
-          "pitch": 0.04134819899506148,
+          "yaw": 1.0500645923542962,
+          "pitch": -0.0034779366638133524,
           "rotation": 0,
-          "target": "0-panorama-coridor"
+          "target": "0-koridor-"
         },
         {
-          "yaw": -1.1570883126810756,
-          "pitch": -0.046266871497845585,
+          "yaw": 0.02695915667054649,
+          "pitch": 0.06668410257633006,
           "rotation": 0,
-          "target": "8-panorama-bedroom-night"
+          "target": "10-living-room-day2"
+        },
+        {
+          "yaw": -1.0726222046295675,
+          "pitch": -0.04003656148995205,
+          "rotation": 0,
+          "target": "8-living-room-night-1-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "8-panorama-bedroom-night",
-      "name": "Panorama Bedroom night",
+      "id": "8-living-room-night-1-",
+      "name": "Living Room night 1 ",
       "levels": [
         {
           "tileSize": 256,
@@ -410,15 +398,134 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -1.011840595789904,
+          "pitch": 0.029128391317410518,
+          "rotation": 0,
+          "target": "7-living-room-day-"
+        },
+        {
+          "yaw": 0.9792839573720222,
+          "pitch": 0.03706960535133064,
+          "rotation": 0,
+          "target": "0-koridor-"
+        },
+        {
+          "yaw": 0.012426882852787813,
+          "pitch": 0.08918668322619894,
+          "rotation": 0,
+          "target": "9-living-room-night-2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-living-room-night-2-",
+      "name": "Living Room night 2 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5631560437276768,
+          "pitch": -0.029558006590299613,
+          "rotation": 0,
+          "target": "10-living-room-day2"
+        },
+        {
+          "yaw": 2.1432745612911575,
+          "pitch": 0.03323889796798518,
+          "rotation": 0,
+          "target": "0-koridor-"
+        },
+        {
+          "yaw": -3.1026051835029165,
+          "pitch": 0.08259833416940232,
+          "rotation": 0,
+          "target": "8-living-room-night-1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-living-room-day2",
+      "name": "Living Room day2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.179730180495252,
+          "pitch": 0.08335621149562655,
+          "rotation": 0,
+          "target": "0-koridor-"
+        },
+        {
+          "yaw": -3.125533463039446,
+          "pitch": 0.0809307462880362,
+          "rotation": 0,
+          "target": "7-living-room-day-"
+        },
+        {
+          "yaw": -1.8096590509769754,
+          "pitch": -0.0016899699286607728,
+          "rotation": 0,
+          "target": "9-living-room-night-2-"
+        }
+      ],
       "infoHotspots": []
     }
   ],
-  "name": "Amsterdam 100",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": false,
-    "viewControlButtons": true
+    "viewControlButtons": false
   }
 };
