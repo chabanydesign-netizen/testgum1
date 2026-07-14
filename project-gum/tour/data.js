705389209e706bcd96237937e8860de8b10f4e30
chabanydesign-netizen <chabanydesign@gmail.com>
2026-07-14 19:33:39 +0300

    вмія

--- project-gum/tour/data.js
+++ project-gum/tour/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-",
-      "name": "Вітальня",
+      "id": "0-01",
+      "name": "01",
       "levels": [
         {
           "tileSize": 256,
@@ -30,24 +30,17 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.2918682916976909,
-          "pitch": 0.0943280359396308,
+          "yaw": -0.03547773580097413,
+          "pitch": 0.7297038368815123,
           "rotation": 0,
-          "target": "1-"
+          "target": "1-02"
         }
       ],
-      "infoHotspots": [
-        {
-          "yaw": -1.5474305738498337,
-          "pitch": 0.07035835752329511,
-          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Відео презентація</font></font>",
-          "text": "Text"
-        }
-      ]
+      "infoHotspots": []
     },
     {
-      "id": "1-",
-      "name": "Спальня",
+      "id": "1-02",
+      "name": "02",
       "levels": [
         {
           "tileSize": 256,
@@ -75,10 +68,98 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.8425277685827837,
-          "pitch": 0.10783873869456961,
+          "yaw": 2.4448091257190496,
+          "pitch": 0.601205310109032,
           "rotation": 0,
-          "target": "0-"
+          "target": "0-01"
+        },
+        {
+          "yaw": -1.3407379884143573,
+          "pitch": 0.3195029947374657,
+          "rotation": 0,
+          "target": "2-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03",
+      "name": "03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1468503373702426,
+          "pitch": 0.03375925505229205,
+          "rotation": 0,
+          "target": "3-02-"
+        },
+        {
+          "yaw": -0.4611305068593534,
+          "pitch": 0.2317689320436518,
+          "rotation": 0,
+          "target": "1-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-02-",
+      "name": "02 копія",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.86592549224655,
+          "pitch": 0.11534223461116966,
+          "rotation": 0,
+          "target": "2-03"
         }
       ],
       "infoHotspots": []
