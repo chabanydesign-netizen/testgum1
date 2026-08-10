4286c96c038a78648c15794ebf020990e6a07f95
chabanydesign-netizen <chabanydesign@gmail.com>
2026-08-10 22:00:19 +0300

    апо

--- kafedralna/tour/data.js
+++ kafedralna/tour/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-1",
-      "name": "1",
+      "id": "0-01",
+      "name": "01",
       "levels": [
         {
           "tileSize": 256,
@@ -28,25 +28,12 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": -0.021509215193123765,
-          "pitch": 0.18879550182933968,
-          "rotation": 0,
-          "target": "2-2"
-        },
-        {
-          "yaw": 1.1148139735245728,
-          "pitch": 0.2310221814658604,
-          "rotation": 0,
-          "target": "1-11"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "1-11",
-      "name": "1.1",
+      "id": "1-02",
+      "name": "02",
       "levels": [
         {
           "tileSize": 256,
@@ -72,52 +59,7 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": 1.1344932194268083,
-          "pitch": 0.15531603582590137,
-          "rotation": 0,
-          "target": "0-1"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-2",
-      "name": "2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 3.139490296759184,
-          "pitch": 0.24510374967503523,
-          "rotation": 0,
-          "target": "0-1"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     }
   ],
