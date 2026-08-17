4e0ab445e07f91cdd1fb8a201fa8e5f9ed84af1c
chabanydesign-netizen <chabanydesign@gmail.com>
2026-08-17 19:14:58 +0300

    счпот

--- lypyny/tour/data.js
+++ lypyny/tour/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-03",
-      "name": "03",
+      "id": "0-",
+      "name": "Вітальня",
       "levels": [
         {
           "tileSize": 256,
@@ -24,23 +24,29 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.13511859322573727,
+        "pitch": 0.009357806790788459,
+        "fov": 1.4841481570173938
       },
       "linkHotspots": [
         {
           "yaw": -0.4857700585409255,
           "pitch": -0.10143771014363345,
           "rotation": 0,
-          "target": "1-01"
+          "target": "1-"
+        },
+        {
+          "yaw": -0.5973919401546315,
+          "pitch": -0.09148382005194122,
+          "rotation": 0,
+          "target": "3--"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-01",
-      "name": "01",
+      "id": "1-",
+      "name": "Спальня",
       "levels": [
         {
           "tileSize": 256,
@@ -67,20 +73,20 @@ var APP_DATA = {
           "yaw": 0.7454963804246262,
           "pitch": 0.01636525885187723,
           "rotation": 0,
-          "target": "2-02"
+          "target": "2-"
         },
         {
           "yaw": 1.6864151591857963,
           "pitch": 0.08617281591407888,
           "rotation": 0,
-          "target": "0-03"
+          "target": "0-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-02",
-      "name": "02",
+      "id": "2-",
+      "name": "Гардероб",
       "levels": [
         {
           "tileSize": 256,
@@ -107,7 +113,41 @@ var APP_DATA = {
           "yaw": 0.05248091977965075,
           "pitch": 0.13463901502296416,
           "rotation": 0,
-          "target": "1-01"
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "Санвузол ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8511376653281175,
+          "pitch": 0.05011879990090584,
+          "rotation": 0,
+          "target": "0-"
         }
       ],
       "infoHotspots": []
